25bc8377623826bb309a04fe8d4e3b41fa79c14e
Joe C <52496811+Jcorb08@users.noreply.github.com>
2022-10-14 18:15:59 +0000

    Json for Uptime Https

---
+++ deploy-commands.js
@@ -0,0 +1,7 @@
+// Since commands only need to be registered once, and updated when the
+// definition (description, options etc) is changed, it's not necessary to 
+// connect a whole client to the gateway or do this on every ready event. As 
+// such, a standalone script using the lighter REST manager is preferred.
+
+//const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+//const mySecret = process.env['Discord_Bot_Secret'];
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-const Discord = require('discord.js');
-
+// Require necessary Discord Classes
+const { Client, GatewayIntentBits}  = require('discord.js');
+// Keep the bot alive!
+var http = require('http');
+const server = http.createServer();
+server.listen(8080);
+// My super secret token
 const mySecret = process.env['Discord_Bot_Secret'];
+
+// Client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+    //GatewayIntentBits.GuildMessages,
+  ],
+});
+
+//client ready run this!
+client.once('ready', () => {
+  console.log("I am " + client.user.username);
+});
+
+// Login to discord with the token
+client.login(mySecret);
+console.log('boo')
+
+server.on('request', (request, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200);
+  var responseJSON = {};
+  if(client.isReady()){
+    responseJSON.message = "I am " + client.user.username;
+    //JavaScript counts in milliseconds
+    var dateTime = new Date(client.readyTimestamp).toLocaleString();
+    responseJSON.readyTimestamp = "I was ready at: " + dateTime;
+    responseJSON.uptime = "I've been Up for: " + client.uptime + 'ms';
+  } else {
+    responseJSON.message = 'The Discord bot is not Ready.';
+  }
+  res.end(JSON.stringify(responseJSON));
+})
+  
+
+
+
+
+
