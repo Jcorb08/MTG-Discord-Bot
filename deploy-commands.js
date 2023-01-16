21c0c6361d14702a3ffceeee0bb51650343cf226
Joe C <52496811+Jcorb08@users.noreply.github.com>
2023-01-16 10:00:33 +0000

    Test Commands Working

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,20 +5,40 @@
 
 //You only need to run node deploy-commands.js once. You should only run it again if you add or edit existing commands.
 
-const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+const { REST, Routes } = require('discord.js');
+// native file system module
+const fs = require('node:fs');
 const mySecret = process.env['DISCORD_BOT_SECRET'];
 const appID = process.env['DISCORD_BOT_APP_ID'];
 const guildID = process.env['DISCORD_BOT_DEV_GUILD_ID'];
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+const commands = [];
+// Grab all the command files from the commands directory must end with .js
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(mySecret);
 
-rest.put(Routes.applicationGuildCommands(appID, guildID), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(appID, guildID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure to catch and log any errors!
+		console.error(error);
+	}
+})();
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-//process.exit();
+// native file system module
+const fs = require('node:fs');
+// native path utility module
+const path = require('node:path');
 // Require necessary Discord Classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 // Keep the bot alive!
 var http = require('http');
 const server = http.createServer();
@@ -17,6 +20,44 @@ const client = new Client({
   ],
 });
 
+// Collection - extends Map Class - stores our commands for execution
+client.commands = new Collection();
+// construct path to commands dir
+const commandsPath = path.join(__dirname, 'commands');
+// reads path and returns array of all file names - makes sure its just .js files
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+// listener to execute code when app receives interaction
+client.on(Events.InteractionCreate, async interaction => {
+  // Not all interactions are slash commands
+  if (!interaction.isChatInputCommand()) return;
+	//console.log(interaction);
+  // get matching command 
+  const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+  // call commands execute method
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
 //client ready run this!
 client.once(Events.ClientReady, c => {
   console.log("I am " + c.user.tag);
@@ -27,6 +68,7 @@ client.on(Events.Error, ( e ) => console.log(e));
 //client.on(Events.Warn, ( e ) => console.log(e));
 //client.on(Events.ShardError, ( e ) => console.log(e));
 
+// Create server to host bot
 server.on('request', (request, res) => {
   res.setHeader("Content-Type", "application/json");
   res.writeHead(200);
