98bab3aabd59660337100f43d02ced2c7a285024
Joe Corbett <josephcorbett@surfaceimpression.com>
2023-05-28 18:30:23 +0100

    add description and emojis to description

--- api/cardEmbed.js
+++ api/cardEmbed.js
@@ -5,7 +5,13 @@ const emojis = require('../emojis/emojis.json')
 function cardEmbed (card, client) {
   // use embed to show card
   const embed = new EmbedBuilder()
-  embed.setColor(Scryfall.colors[card.colors[0]] ?? 'LightGrey')
+  if(card.colors?.length > 0){
+    embed.setColor(Scryfall.colors[card.colors[0]])
+  } else if (card.color_identity?.length>0){
+    embed.setColor(Scryfall.colors[card.color_identity[0]])
+  } else {
+    embed.setColor('LightGrey')
+  }
   let mana_emojis = '';
   if (card.mana_cost){
     mana_emojis += ' '
@@ -20,6 +26,11 @@ function cardEmbed (card, client) {
   embed.setTitle(card.name + mana_emojis)
   embed.setURL(card.scryfall_uri)
   embed.setDescription(card.type_line)
+  if(card.oracle_text){
+    let description = card.oracle_text
+    description = description.replaceAll(/{([A-Za-z0-9]+)}/g,(match, p1, offset) => emojis["mana"+p1.toLowerCase()])
+    embed.addFields({name:' ',value:description})
+  }
   // embed.setAuthor({ name: client.user.tag, iconURL: client.user.avatarURL() })
   embed.setAuthor({ name: card.artist })
   if (card.released_at) {
